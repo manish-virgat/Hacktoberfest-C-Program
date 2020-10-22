2266f99ed3980fe450dcdf5005fbe26e03f929fc
guitld <seuto8199@gmail.com>
2020-10-22 18:48:55 -0300

    Added iteractive and recursive factorial.c

--- factorial.c
+++ factorial.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long calculateFactorial(long number, long currentResult){
+long recursiveCalculateFactorial(long number, long currentResult){
 
     if (number == 0) return currentResult;
 
-    return calculateFactorial(number - 1, number * currentResult);
+    return recursiveCalculateFactorial(number - 1, number * currentResult);
+
+}
+
+long calculateFactorial(long number){
+
+    long result = 1;
+
+    for (int i = 1; i <= number; i++) result *= i;
+    
+    return result;
 
 }
 
 int main(int arc, char *argv[]){
 
-    long result = calculateFactorial(atol(argv[1]), 1);
+    long result = calculateFactorial(atol(argv[1]));
     printf("The factorial of %li is %li\n", atol(argv[1]), result);
 
     return 0;
